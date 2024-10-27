42ca49c013f04de99f819f0b754107851a418584
baraa assaf <108326090+BaraaAssaf@users.noreply.github.com>
2024-10-26 22:17:10 -0400

    Update [id].js

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -10,7 +10,6 @@ export default function EditProductPage() {
 
     useEffect(() => {
         const fetchProduct = async () => {
-            console.log(id)
             if (id) {
                 const response = await fetchProducts({ id });
                 setProduct(response.data[0]);
