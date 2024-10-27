d85ef38017962059ececbd547671e6e9eabc3db8
BaraaAssaf <bmassaf1997@gmail.com>
2024-10-26 22:14:10 -0400

    fix

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -10,6 +10,7 @@ export default function EditProductPage() {
 
     useEffect(() => {
         const fetchProduct = async () => {
+            console.log(id)
             if (id) {
                 const response = await fetchProducts({ id });
                 setProduct(response.data[0]);
